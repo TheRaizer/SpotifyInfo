421f0dcd057ece606770616aa2d74b9a1e7f690e
Aidan <aidan.fu000@gmail.com>
2021-05-31 00:48:40 -0400

    implemented the ability to obtain auth code from spotify web api

--- src/public/spotify-api-connect.js
+++ src/public/spotify-api-connect.js
@@ -1,4 +1,8 @@
 const authEndpoint = "https://accounts.spotify.com/authorize";
+const tokenEndpoint = "https://accounts.spotify.com/api/token";
+
+var authCode = null;
+
 // Replace with your app's client ID, redirect URI and desired scopes
 const clientId = "434f5e9f442a4e4586e089a33f65c857";
 const redirectUri = "http://localhost:3000";
@@ -24,35 +28,46 @@ const scopes = [
   "user-follow-modify",
 ];
 
-// authentification url as shown in https://developer.spotify.com/documentation/general/guides/authorization-guide/#:~:text=A%20token%20that%20can%20be,access%20token%20will%20be%20returned.
+// Authentification url as shown in https://developer.spotify.com/documentation/general/guides/authorization-guide/#:~:text=A%20token%20that%20can%20be,access%20token%20will%20be%20returned.
 // underneath the code flow image's, first table. Where it states 'A typical request is the GET request of the /authorize endpoint, followed by the query:'
 const authURL = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
   "%20"
-)}&response_type=token&show_dialog=true`;
+)}&response_type=code&show_dialog=true`;
 
-// Create anchor element.
-var a = document.createElement("a");
-// Create the text node for anchor element.
-var link = document.createTextNode("Login To Spotify");
-// Append the text node to anchor element.
-a.appendChild(link);
-// Set the title.
-a.title = "Login To Spotify";
-a.id = "spotify-login";
-// Set the href property.
-a.href = authURL;
-// Append the anchor element to the body.
-document.getElementById("spotify-container").appendChild(a);
+const tokenURL = `${tokenEndpoint}?grant_type=authorization_code&code=${authCode}&redirect_uri=${redirectUri}`;
 
-// Get the hash of the url
-const hash = window.location.hash
-  .substring(1)
-  .split("&")
-  .reduce(function (initial, item) {
-    if (item) {
-      var parts = item.split("=");
-      initial[parts[0]] = decodeURIComponent(parts[1]);
-    }
-    return initial;
-  }, {});
-window.location.hash = "";
+function createSpotifyLoginButton() {
+  // Create anchor element.
+  var a = document.createElement("a");
+  // Create the text node for anchor element.
+  var link = document.createTextNode("Login To Spotify");
+  // Append the text node to anchor element.
+  a.appendChild(link);
+  // Set the title.
+  a.title = "Login To Spotify";
+  a.id = "spotify-login";
+  // Set the href property.
+  a.href = authURL;
+  // Append the anchor element to the body.
+  document.getElementById("spotify-container").appendChild(a);
+}
+createSpotifyLoginButton();
+
+function checkForCode() {
+  // create a parameter searcher in the URL after '?' which holds the requests body parameters
+  const urlParams = new URLSearchParams(window.location.search);
+
+  // Get the code from the parameter called 'code' in the url which
+  // hopefully came back from the spotify GET request otherwise it is null
+  authCode = urlParams.get("code");
+
+  console.log(authCode);
+
+  // because the code has been obtained we want to change the url
+  // so it doesn't have the code without refreshing the page
+  window.history.pushState(null, null, "/");
+}
+
+if (authCode == null) {
+  checkForCode();
+}
