4624b339037764a7a3366f3676e7f51d2dec1ae3
Aidan <aidan.fu000@gmail.com>
2021-08-13 00:21:30 -0400

    fix bug where resize would change height and transform instead of just width

--- src/public/pages/playlists-page/playlists.js
+++ src/public/pages/playlists-page/playlists.js
@@ -47,18 +47,9 @@ const resizeActions = (function () {
         edges: { top: false, left: false, bottom: false, right: true },
         listeners: {
           move: function (event) {
-            let { x, y } = event.target.dataset;
-
-            x = (parseFloat(x) || 0) + event.deltaRect.left;
-            y = (parseFloat(y) || 0) + event.deltaRect.top;
-
             Object.assign(event.target.style, {
               width: `${event.rect.width}px`,
-              height: `${event.rect.height}px`,
-              transform: `translate(${x}px, ${y}px)`,
             });
-
-            Object.assign(event.target.dataset, { x, y });
           },
         },
       })
