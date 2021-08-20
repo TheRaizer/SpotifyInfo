6bf52fcb495e6428c81785786635b5e2f22e73cb
Aidan <aidan.fu000@gmail.com>
2021-08-19 22:59:06 -0400

    create test pubsub aggregator pattern

---
+++ src/pubsub/examples/example-event-arg.js
@@ -0,0 +1,7 @@
+export default class ExampleEventArg {
+  constructor(data1, data2, data3) {
+    this.data1 = data1;
+    this.data2 = data2;
+    this.data3 = data3;
+  }
+}
---
+++ src/pubsub/examples/example-process.js
@@ -0,0 +1,19 @@
+import EventAggregator from "../aggregator.js";
+import ExampleEventArg from "./example-event-arg.js";
+
+const eventAggregator = new EventAggregator();
+
+/** An example event.
+ *
+ * @param {ExampleEventArg} exampleEventArg
+ */
+function exampleEvent(exampleEventArg) {
+  console.log(exampleEventArg);
+}
+
+// subscribe event that will take in ExampleEventArg as an argument.
+eventAggregator.subscribe(ExampleEventArg.name, exampleEvent);
+
+// publish all events that take ExampleEventArg as an argument.
+eventAggregator.publish(new ExampleEventArg(1, 2, 3));
+eventAggregator.publish(new ExampleEventArg(66, 233, 122));
